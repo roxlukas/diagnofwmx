47788d195eb38c3d68ae136e6bfc94450c076871
Lukasz Pozniak <pozniak.lukasz@gmail.com>
2016-11-14 12:27:34 +0100

    development update

--- diagnofwmx.php
+++ diagnofwmx.php
@@ -11,6 +11,18 @@ $content = file_get_contents($file);
 if (empty($content)) die ("*** Test scenario '" . $argv[1] . "' is empty.");
 $scenario = preg_split('/\r|\r\n|\n/', $content);
 
+//the whole test result log will be stored here
+$results='';
+
 foreach ($scenario as $line) {
-    echo ("*** Scenario line: $line \r\n");
+    if (!empty($line)) {
+        echo ("*** Scenario line: $line \r\n");
+        $test = explode('|', $line);
+        if ($test[1] == 'dns') {
+            exec("dig ${test[2]} ${test[3]} ",$out);
+            $results .= $out . "\r\n";
+        } 
+    }
 }
+
+echo($results);
