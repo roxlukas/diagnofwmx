788519209fcf2bdb2d698bb776273d942842fe92
Lukasz Pozniak <pozniak.lukasz@gmail.com>
2016-11-14 10:47:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>About</h1>
+
+This project is an automated diagnostic script for checking network connectivity with different online games servers.
+It will try to resolve all domains normally resolved by game client, and then it will try to ping/traceroute to all discovered addresses.
+Whole output is saved into a log file, which can be later sent to ISP or game's developers.
+
+<h3>Donations are welcome</h3>
+
+If you'd like to support the development, feel free to do so: https://www.paypal.me/roxlukas
---
+++ diagnofwmx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# PHP CLI script launcher
+#
+
+PHPPKG=php7.0-cli
+ISPHPINST=$(dpkg-query -W -f='${Status}' $PHPPKG 2>/dev/null | grep -c "ok installed")
+PHPFILE=diagnofwmx.php
+PHPURL=https://raw.githubusercontent.com/roxlukas/diagnofwmx/master/$PHPFILE
+
+if [ "$ISPHPINST" -ne "1" ]; then
+    echo "Installing PHP..."
+    sudo apt -y install $PHPPKG
+else
+    echo "PHP is already installed"
+fi
+
+echo "Downloading the diagnostics script from $PHPURL"
+wget $PHPURL
+
+echo "Starting the diagnostic script $PHPFILE"
+php $PHPFILE $1 $2 $3 $4
