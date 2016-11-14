6cef00f92c7738e2fbc99af34cb5dbd06baab512
Lukasz Pozniak <pozniak.lukasz@gmail.com>
2016-11-14 12:09:44 +0100

    development update

--- diagnofwmx.php
+++ diagnofwmx.php
@@ -2,10 +2,15 @@
 $baseurl = "https://raw.githubusercontent.com/roxlukas/diagnofwmx/master";
 
 if ($argc < 1) {
-    die("*** You must specify a game-specific test script. Check README.md for details.");
+    die("*** You must specify a game-specific test scenario. Check README.md for details.");
 }
 
 $file = $baseurl . '/games/' . $argv[1] . '.txt';
-echo ("*** Test script: $file \r\n");
+echo ("*** Test scenario: $file \r\n");
 $content = file_get_contents($file);
-echo ("*** Contents: $content \r\n");
+if (empty($content)) die ("*** Test scenario '" . $argv[1] . "' is empty.");
+$scenario = preg_split('/\r|\r\n|\n/', $content);
+
+foreach ($scenario as $line) {
+    echo ("*** Scenario line: $line \r\n");
+}
